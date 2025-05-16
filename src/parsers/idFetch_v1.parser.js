adb8285d48c1c4d945de3b81d5dee192a1e869f6
rishabh-star <risingmerta@gmail.com>
2025-05-16 09:22:22 +0530

    first commit

--- src/parsers/idFetch_v1.parser.js
+++ src/parsers/idFetch_v1.parser.js
@@ -12,7 +12,7 @@ export async function fetchServerData_v1(id) {
     const serverData = $("div.ps_-block > div.ps__-list > div.server-item")
       .filter((_, ele) => {
         const name = $(ele).find("a.btn").text();
-        return name === "HD-1" || name === "HD-2";
+        return name === "HD-3" || name === "HD-2";
       })
       .map((_, ele) => ({
         name: $(ele).find("a.btn").text(),
